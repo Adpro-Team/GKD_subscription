5045012795ec339377971a98fef6083ab0313ca2
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-19 23:56:01 +0800

    perf(集换社): 开屏广告

--- src/apps/com.jihuanshe.ts
+++ src/apps/com.jihuanshe.ts
@@ -5,21 +5,12 @@ export default defineAppConfig({
   name: '集换社',
   groups: [
     {
+      key: 0,
       name: '开屏广告',
       desc: '点击【跳过】',
-      key: 0,
-      rules: [
-        {
-          matches: [
-            '[id="com.jihuanshe:id/adContainer"] > FrameLayout > FrameLayout > View',
-          ],
-          snapshotUrls: ['https://i.gkd.li/i/14303879'],
-          exampleUrls: [
-            'https://m.gkd.li/81077248/524e518e-7b7e-4ef3-934b-427feefaa185',
-          ],
-          activityIds: ['com.jihuanshe.ui.page.SplashActivity'],
-        },
-      ],
+      quickFind: true,
+      rules: '@View <2 FrameLayout <2 FrameLayout < [vid="adContainer"]',
+      snapshotUrls: 'https://i.gkd.li/i/14303879',
     },
   ],
 });
