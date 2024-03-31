b54af6728c162efb52723c388d60aa1e6922bd70
TianMiao <tianmiao.work@foxmail.com>
2024-03-31 15:05:00 +0800

    feat(彩云天气): 全屏广告、局部广告、功能类-点击收起顶部工具栏

--- src/apps/com.nowcasting.activity.ts
+++ src/apps/com.nowcasting.activity.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '弹窗广告_VIP',
+      name: '全屏广告-VIP弹窗广告',
       quickFind: true,
       activityIds: 'com.nowcasting.activity.WeatherActivity',
       rules: [
@@ -19,7 +19,7 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '弹窗广告_外部应用',
+      name: '弹窗广告-外部应用',
       quickFind: true,
       activityIds: 'com.nowcasting.activity.WeatherActivity',
       rules: [
@@ -34,27 +34,84 @@ export default defineAppConfig({
     },
     {
       key: 3,
-      name: '插页广告',
+      name: '全屏广告-弹窗广告',
       quickFind: true,
       activityIds: 'com.nowcasting.activity.WeatherActivity',
       rules: [
         {
           key: 0,
-          matches: ['[id="com.nowcasting.activity:id/jad_feed_close"]'],
-          snapshotUrls: 'https://i.gkd.li/i/13690826',
+          matches:
+            '[id="android:id/content"] >n FrameLayout[childCount>5] > FrameLayout[childCount=1] > ImageView',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13405131',
+            'https://i.gkd.li/i/14814345',
+          ],
         },
         {
           key: 1,
-          matches: ['[id="com.nowcasting.activity:id/ll_close"]'],
+          quickFind: true,
+          matches:
+            'ImageView < @ViewGroup[clickable=true] <<n ViewGroup +n ViewGroup [text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/i/14814374',
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '分段广告-卡片广告',
+      quickFind: true,
+      activityIds: 'com.nowcasting.activity.WeatherActivity',
+      rules: [
+        {
+          key: 1,
+          matches: '[id="com.nowcasting.activity:id/ll_close"]',
           snapshotUrls: 'https://i.gkd.li/i/13690822',
         },
         {
           preKeys: 1,
           key: 2,
-          matches: ['[id="com.nowcasting.activity:id/ll_uninterested"]'],
+          matches: '[id="com.nowcasting.activity:id/ll_uninterested"]',
           snapshotUrls: 'https://i.gkd.li/i/13690830',
         },
       ],
     },
-  ],
+    {
+      key: 5,
+      name: '局部广告-卡片广告',
+      desc: '点击关闭',
+      quickFind: true,
+      activityIds: 'com.nowcasting.activity.WeatherActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.nowcasting.activity:id/jad_feed_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/13690826',
+        },
+        {
+          key: 1,
+          matches:
+            '@FrameLayout[visibleToUser=true] +2 * > [text="广告"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/14814348',
+        },
+      ],
+    },
+    {
+      key: 6,
+      name: '功能类-点击收起顶部工具栏',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.nowcasting.activity.WeatherActivity',
+          matches:
+            'ViewGroup[childCount=2][visibleToUser=true] > [vid="fl_arrow"][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/5bdf4f45-d66d-441e-a2ce-f32d5fc40796',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14814347', // 收起前
+            'https://i.gkd.li/i/14814412', // 收起后
+          ],
+        },
+      ],
+    }
+  ]
 });
